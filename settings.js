42c09fa99268f302a2e6261e62d30cdd7b9579dc
Jake <505e06b2@gmail.com>
2022-05-01 17:27:00 +0100

    Clear out default settings

--- settings.js
+++ settings.js
@@ -8,9 +8,9 @@ module.exports = {
 	//Anything undefined will not overwrite the settings saved while browsing (query.txt)
 	parameters: { //https://github.com/505e06b2/media_player/wiki/Query-Parameters
 		bgcolour: undefined,
-		fgcolour: "#ffbf00",
-		dockcolour: "#4e3b00",
-		playlists: ["LhgTByqc", "ntNBc5E2"],
+		fgcolour: undefined,
+		dockcolour: undefined,
+		playlists: [],
 		gain: undefined,
 		shuffle: undefined,
 		repeat: undefined
